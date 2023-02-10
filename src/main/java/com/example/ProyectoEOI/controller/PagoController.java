d6c36d3faa9fc73658890572243116ebbfe5edcc
CPL <carlos@centropyme.com>
2023-02-10 18:59:22 +0100

    Cambios en el html y en el modelo de viaje

--- src/main/java/com/example/ProyectoEOI/controller/PagoController.java
+++ src/main/java/com/example/ProyectoEOI/controller/PagoController.java
@@ -8,13 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PagoController {
     private final PagoService service;
 
-    @Autowired
     public PagoController(PagoService service) {
         this.service = service;
     }
@@ -25,7 +26,6 @@ public class PagoController {
         interfaz.addAttribute("precio", pagoDto);
         return "precio";
     }
-
 
 }
 
